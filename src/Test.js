3cc6d4be217428300c75f28e6bcb79d55d51ef49
Jelle <jelle.keulemans@gmail.com>
2021-11-18 17:59:33 +0100

    now working with events and proper hooks

--- src/Test.js
+++ src/Test.js
@@ -39,11 +39,17 @@ function Test() {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <EllipsisVectorLayer
+      {/* <EllipsisVectorLayer
         blockId='b8468235-31b5-4959-91a4-0e52a1d4feb6'
         layerId='44be2542-d20d-457b-b003-698d048d2c6c'
         radius={3}
         mapRef={map}
+      /> */}
+
+      <EllipsisVectorLayer
+        blockId='1a24a1ee-7f39-4d21-b149-88df5a3b633a'
+        layerId='45c47c8a-035e-429a-9ace-2dff1956e8d9'
+        onFeatureClick={(feature, layer) => console.log(feature)}
       />
 
       {/* <EllipsisVectorLayer
--- src/lib/EllipsisVectorLayer.js
+++ src/lib/EllipsisVectorLayer.js
@@ -1,118 +1,108 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-import { GeoJSON, CircleMarker, Marker } from 'react-leaflet';
+import { GeoJSON, CircleMarker, Marker, useMapEvents } from 'react-leaflet';
 
 import EllipsisApi from './EllipsisApi';
 
-export class EllipsisVectorLayer extends React.PureComponent {
+const EllipsisVectorLayer = props => {
 
-  constructor(props, context) {
-    super(props);
-    this.tiles = [];
-    this.zoom = 1;
-    this.state = { cache: [] };
-  }
+  const [state, setState] = useState({
+    cache: [],
+    tiles: [],
+    zoom: 1,
+    isLoading: false,
+    nextPageStart: undefined,
+    lastChanged: Date.now()
+  });
 
-  // componentDidMount = () => {
-  //   console.log(this.props);
-  //   if(!this.props.mapRef) {
-  //     console.log(JSON.stringify(this.props));
-  //     return console.error(`No map reference was given to the ellipsis vector layer`);
-  //   }
-  // };
-
-  componentDidUpdate = (prevProps, prevState) => {
-    if(!prevProps.mapRef && this.props.mapRef) {
-      this.handleViewportUpdate();
-      this.props.mapRef.on('moveEnd', () => {
-        this.handleViewportUpdate();
-        console.log('end move');
-      });
-      this.props.mapRef.on('zoom', () => this.handleViewportUpdate());
+  const map = useMapEvents({
+    moveend: () => {
+      handleViewportUpdate();
+    },
+    zoomend: () => {
+      handleViewportUpdate();
     }
-  }
+  });
 
-  // componentDidUpdate = (prevProps, prevState) => {
-  //   clearInterval(this.gettingVectorsInterval);
-  //   this.gettingVectorsInterval = undefined;
-  //   this.setState({cache: []}, () => this.handleViewportUpdate());
-  // };
+  useEffect(() => {
+    if(state.isLoading) return;
+    loadStep().then(loadedSomething => {
+      if(loadedSomething){
+        state.lastChanged = Date.now();
+        setState({...state});
+      }
+    });
+  }, [state])
 
-  componentWillUnmount = () => {
-    clearInterval(this.gettingVectorsInterval);
-  };
-
-  handleViewportUpdate = () => {
-    const viewport = this.getMapBounds();
+  const handleViewportUpdate = async () => {
+    const viewport = getMapBounds();
     if (!viewport) return;
-    this.zoom = Math.max(Math.min(this.maxZoom, viewport.zoom - 2), 0);
-    this.tiles = this.boundsToTiles(viewport.bounds, this.zoom);
+    state.zoom = Math.max(Math.min(props.maxZoom, viewport.zoom - 2), 0);
+    state.tiles = boundsToTiles(viewport.bounds, state.zoom);
 
-    if(this.gettingVectorsInterval) return;
-
-    this.gettingVectorsInterval = setInterval(async () => {
-        if(this.isLoading) return;
-
-        const loadedSomething = await this.loadStep();
-        if(!loadedSomething) {
-            clearInterval(this.gettingVectorsInterval);
-            this.gettingVectorsInterval = undefined;
-            return;
-        }
-        this.setState({cache: []});
-    }, 100);
+    if(state.isLoading) return;
+    const loadedSomething = await loadStep();
+    if(loadedSomething){
+      state.lastChanged = Date.now();
+      setState({...state});
+    }
   };
 
-  loadStep = async () => {
-      this.isLoading = true;
-      if(this.props.loadAll) {
-          const cachedSomething = await this.getAndCacheAllGeoJsons();
-          this.isLoading = false;
+  const loadStep = async () => {
+      state.isLoading = true;
+      // console.log('loading');
+      if(props.loadAll) {
+          const cachedSomething = await getAndCacheAllGeoJsons();
+          state.isLoading = false;
+          // console.log('done loading');
           return cachedSomething;
       }
 
-      this.ensureMaxCacheSize();
-      const cachedSomething = await this.getAndCacheGeoJsons();
-      this.isLoading = false;
+      ensureMaxCacheSize();
+      const cachedSomething = await getAndCacheGeoJsons();
+      state.isLoading = false;
+      // console.log('done loading');
       return cachedSomething;
   };
 
-  ensureMaxCacheSize = () => {
-      const keys = Object.keys(this.state.cache);
-      if (keys.length > this.props.maxTilesInCache) {
-          const dates = keys.map((k) => this.state.cache[k].date).sort();
+  
+
+  const ensureMaxCacheSize = () => {
+      const keys = Object.keys(state.cache);
+      if (keys.length > props.maxTilesInCache) {
+          const dates = keys.map((k) => state.state.cache[k].date).sort();
           const clipValue = dates[9];
           keys.forEach((key) => {
-              if (this.state.cache[key].date <= clipValue) {
-                  delete this.state.cache[key];
+              if (state.cache[key].date <= clipValue) {
+                  delete state.cache[key];
               }
           });
       }
   };
 
-  getAndCacheAllGeoJsons = async () => {
-      if(this.nextPageStart === 4)
+  const getAndCacheAllGeoJsons = async () => {
+      if(state.nextPageStart === 4)
           return false;
       
       const body = {
-          pageStart: this.nextPageStart,
-          mapId: this.blockId,
-          returnType: this.centerPoints ? "center" : "geometry",
-          layerId: this.layerId,
+          pageStart: state.nextPageStart,
+          mapId: props.blockId,
+          returnType: props.centerPoints ? "center" : "geometry",
+          layerId: props.layerId,
           zip: true,
-          pageSize: Math.min(3000, this.pageSize),
-          styleId: this.styleId
+          pageSize: Math.min(3000, props.pageSize),
+          styleId: props.styleId
       };
 
       try {
-          const res = await EllipsisApi.post("/geometry/get", body, {token: this.props.token});
-          this.nextPageStart = res.nextPageStart;
+          const res = await EllipsisApi.post("/geometry/get", body, {token: props.token});
+          state.nextPageStart = res.nextPageStart;
           if(!res.nextPageStart) 
-              this.nextPageStart = 4; //EOT
+              state.nextPageStart = 4; //EOT
           if(res.result && res.result.features) {
               res.result.features.forEach(x => {
-                  this.styleGeoJson(x, this.props.lineWidth, this.props.radius);
-                  this.state.cache.push(x);
+                  styleGeoJson(x, props.lineWidth, props.radius);
+                  state.cache.push(x);
               });
           }
       } catch {
@@ -121,45 +111,45 @@ export class EllipsisVectorLayer extends React.PureComponent {
       return true;
   };
 
-  getAndCacheGeoJsons = async () => {
+  const getAndCacheGeoJsons = async () => {
       const date = Date.now();
       //create tiles parameter which contains tiles that need to load more features
-      const tiles = this.tiles.map((t) => {
-          const tileId = this.getTileId(t);
+      const tilesParam = state.tiles.map((t) => {
+          const tileId = getTileId(t);
 
           //If not cached, always try to load features.
-          if(!this.state.cache[tileId]) 
+          if(!state.cache[tileId]) 
               return { tileId: t}
 
-          const pageStart = this.state.cache[tileId].nextPageStart;
+          const pageStart = state.cache[tileId].nextPageStart;
 
           //TODO in other packages we use < instead of <=
           //Check if tile is not already fully loaded, and if more features may be loaded
-          if(pageStart && this.state.cache[tileId].amount <= this.props.maxFeaturesPerTile && this.state.cache[tileId].size <= this.props.maxMbPerTile)
+          if(pageStart && state.cache[tileId].amount <= props.maxFeaturesPerTile && state.cache[tileId].size <= props.maxMbPerTile)
               return { tileId: t, pageStart }
 
           return null;
       }).filter(x => x);
 
-      if(tiles.length === 0) return false;
+      if(tilesParam.length === 0) return false;
 
       const body = {
-          mapId: this.props.blockId,
-          returnType: this.props.centerPoints ? "center" : "geometry",
-          layerId: this.props.layerId,
+          mapId: props.blockId,
+          returnType: props.centerPoints ? "center" : "geometry",
+          layerId: props.layerId,
           zip: true,
-          pageSize: Math.min(3000, this.props.pageSize),
-          styleId: this.props.styleId,
-          propertyFilter: (this.props.filter && this.props.filter > 0) ? this.props.filter : null,
+          pageSize: Math.min(3000, props.pageSize),
+          styleId: props.styleId,
+          propertyFilter: (props.filter && props.filter > 0) ? props.filter : null,
       };
 
       //Get new geometry for the tiles
       let result = [];
       const chunkSize = 10;
-      for (let k = 0; k < tiles.length; k += chunkSize) {
-          body.tiles = tiles.slice(k, k + chunkSize);
+      for (let k = 0; k < tilesParam.length; k += chunkSize) {
+          body.tiles = tilesParam.slice(k, k + chunkSize);
           try {
-              const res = await EllipsisApi.post("/geometry/tile", body, {token: this.props.token});
+              const res = await EllipsisApi.post("/geometry/tile", body, {token: props.token});
               result = result.concat(res);
           } catch {
               return false;
@@ -167,11 +157,11 @@ export class EllipsisVectorLayer extends React.PureComponent {
       }
       
       //Add newly loaded data to cache
-      for (let j = 0; j < tiles.length; j++) {
-          const tileId = this.getTileId(tiles[j].tileId);
+      for (let j = 0; j < tilesParam.length; j++) {
+          const tileId = getTileId(tilesParam[j].tileId);
 
-          if (!this.state.cache[tileId]) {
-              this.state.cache[tileId] = {
+          if (!state.cache[tileId]) {
+              state.cache[tileId] = {
                   size: 0,
                   amount: 0,
                   elements: [],
@@ -180,23 +170,23 @@ export class EllipsisVectorLayer extends React.PureComponent {
           }
 
           //set tile info for tile in this.
-          const tileData = this.state.cache[tileId];
+          const tileData = state.cache[tileId];
           tileData.date = date;
           tileData.size = tileData.size + result[j].size;
           tileData.amount = tileData.amount + result[j].result.features.length;
           tileData.nextPageStart = result[j].nextPageStart;
-          result[j].result.features.forEach(x => this.styleGeoJson(x, this.props.lineWidth, this.props.radius));
+          result[j].result.features.forEach(x => styleGeoJson(x, props.lineWidth, props.radius));
           tileData.elements = tileData.elements.concat(result[j].result.features);
 
       }
       return true;
   };
 
-  getTileId = (tile) => `${tile.zoom}_${tile.tileX}_${tile.tileY}`;
+  const getTileId = (tile) => `${tile.zoom}_${tile.tileX}_${tile.tileY}`;
 
-  getFeatureId = (feature, index = 0) => `${feature.properties.id}_${this.props.centerPoints ? 'center' : 'geometry'}_${this.props.styleId}_${index}`;
+  const getFeatureId = (feature, index = 0) => `${feature.properties.id}_${props.centerPoints ? 'center' : 'geometry'}_${props.styleId ? props.styleId : 'nostyle'}_${index}`;
 
-  styleGeoJson = (geoJson, weight, radius) => {
+  const styleGeoJson = (geoJson, weight, radius) => {
       if(!geoJson || !geoJson.geometry || !geoJson.geometry.type || !geoJson.properties) return;
 
       const type = geoJson.geometry.type;
@@ -226,7 +216,7 @@ export class EllipsisVectorLayer extends React.PureComponent {
       else properties.style.weight = weight;
   };
 
-  boundsToTiles = (bounds, zoom) => {
+  const boundsToTiles = (bounds, zoom) => {
       const xMin = Math.max(bounds.xMin, -180);
       const xMax = Math.min(bounds.xMax, 180);
       const yMin = Math.max(bounds.yMin, -85);
@@ -266,10 +256,10 @@ export class EllipsisVectorLayer extends React.PureComponent {
       return tiles;
   };
 
-  getMapBounds = () => {
-      if (!this.props.mapRef) return;
-      const screenBounds = this.props.mapRef.getBounds();
-      const zoom = this.props.mapRef.getZoom();
+  const getMapBounds = () => {
+      if (!map) return;
+      const screenBounds = map.getBounds();
+      const zoom = map.getZoom();
       let bounds = {
           xMin: screenBounds.getWest(),
           xMax: screenBounds.getEast(),
@@ -277,21 +267,25 @@ export class EllipsisVectorLayer extends React.PureComponent {
           yMax: screenBounds.getNorth(),
       };
       //Mapbox uses 512x512 tiles, and ellipsis uses 256x256 tiles. So increase zoom with 1. 'zoom256 = zoom512 + 1'
-      return { bounds: bounds, zoom: parseInt(zoom + 1, 10) };
+      return { bounds, zoom };
   };
 
-  render = () => {
-    if (!this.tiles || this.tiles.length === 0) return <></>;
-
+  const render = () => {
+    console.log('render');
+    if (!state.tiles || state.tiles.length === 0) return <></>;
     let features;
-    if(this.props.loadAll) {
-        features = this.state.cache;
+    if(props.loadAll) {
+        features = state.cache;
     } else {
-        features = this.tiles.flatMap((t) => {
-            const geoTile = this.state.cache[this.getTileId(t)];
+        features = state.tiles.flatMap((t) => {
+            const geoTile = state.cache[getTileId(t)];
             return geoTile ? geoTile.elements : [];
         });
     }
+
+    console.log(features.filter(x => 
+      features.filter(y => y.properties.id === x.properties.id).length > 1
+    ));
 
     return <>{features.flatMap(feature => {
       const type = feature.geometry.type;
@@ -299,12 +293,12 @@ export class EllipsisVectorLayer extends React.PureComponent {
       if (type.endsWith('Polygon') || type.endsWith('LineString')) {
         return [
           <GeoJSON
-            key={this.getFeatureId(feature)}
+            key={getFeatureId(feature)}
             data={feature}
             style={feature.properties.style}
-            interactive={this.props.onFeatureClick ? true : false}
-            onEachFeature={!this.props.onFeatureClick ? undefined : (feature, layer) => 
-                layer.on('click', () => this.props.onFeatureClick(feature, layer))
+            interactive={props.onFeatureClick ? true : false}
+            onEachFeature={!props.onFeatureClick ? undefined : (feature, layer) => 
+                layer.on('click', () => props.onFeatureClick(feature, layer))
             }
           />,
         ];
@@ -314,29 +308,66 @@ export class EllipsisVectorLayer extends React.PureComponent {
         //Ensure that it's always an array of coordinates.
         if(!type.startsWith('Multi')) coordinates = [coordinates];
 
-        return coordinates.map((coordinate, i) => this.useMarkers ? 
+        return coordinates.map((coordinate, i) => props.useMarkers ? 
           <Marker
-            key={this.getFeatureId(feature, i)}
+            key={getFeatureId(feature, i)}
             position={[coordinate[1], coordinate[0]]}
-            interactive={this.props.onFeatureClick ? true : false}
-            onClick={!this.props.onFeatureClick ? undefined : (e) => this.props.onFeatureClick(feature, e)}
+            interactive={props.onFeatureClick ? true : false}
+            onClick={!props.onFeatureClick ? undefined : (e) => props.onFeatureClick(feature, e)}
           /> :
           <CircleMarker
-            key={this.getFeatureId(feature, i)}
+            key={getFeatureId(feature, i)}
             center={[coordinate[1], coordinate[0]]}
             color={feature.properties.style.color}
             opacity={feature.properties.style.fillOpacity}
             radius={feature.properties.style.radius}
             weight={feature.properties.style.weight}
-            interactive={this.props.onFeatureClick ? true : false}
-            onClick={!this.props.onFeatureClick ? undefined : (e) => this.props.onFeatureClick(feature, e)}
+            interactive={props.onFeatureClick ? true : false}
+            onClick={!props.onFeatureClick ? undefined : (e) => props.onFeatureClick(feature, e)}
           />
         )
       }
       return [];
-    })}</>
+    })}</> 
   }
+
+  handleViewportUpdate();
+
+  return render();
 }
+
+  // componentDidMount = () => {
+  //   console.log(this.props);
+  //   if(!this.props.mapRef) {
+  //     console.log(JSON.stringify(this.props));
+  //     return console.error(`No map reference was given to the ellipsis vector layer`);
+  //   }
+  // };
+
+//   componentDidUpdate = (prevProps, prevState) => {
+//     if(!prevProps.mapRef && this.props.mapRef) {
+//       this.handleViewportUpdate();
+//       this.props.mapRef.on('moveEnd', () => {
+//         this.handleViewportUpdate();
+//         console.log('end move');
+//       });
+//       this.props.mapRef.on('zoom', () => this.handleViewportUpdate());
+//     }
+//   }
+
+//   // componentDidUpdate = (prevProps, prevState) => {
+//   //   clearInterval(this.gettingVectorsInterval);
+//   //   this.gettingVectorsInterval = undefined;
+//   //   this.setState({cache: []}, () => this.handleViewportUpdate());
+//   // };
+
+//   componentWillUnmount = () => {
+//     clearInterval(this.gettingVectorsInterval);
+//   };
+
+
+
+// }
 
 EllipsisVectorLayer.defaultProps = {
   pageSize: 25,
